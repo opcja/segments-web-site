62048e2ada79ef738c3298c3fc3ee6537e1e90ff
Piotr <piotr@iMac-Piotr.local>
2024-07-27 09:49:21 +0200

    work on responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -373,9 +373,9 @@ const IndexPage = () => {
     if (window.innerWidth < 768 && window.innerHeight > 760) {
       document.querySelector(".page-wrapper").style.height = `${window.innerHeight}px`;
       document.querySelectorAll(".content-box").forEach((item) => {
-        item.style.height = `${window.innerHeight - 242}px`;
+        item.style.height = `${window.innerHeight - 250}px`;
       });
-      document.querySelector(".list-item.active").style.height = `${window.innerHeight - 242}px`;
+      document.querySelector(".list-item.active").style.height = `${window.innerHeight - 250}px`;
     }
     
     listItems.forEach((item) => {
@@ -385,7 +385,7 @@ const IndexPage = () => {
           if (window.innerWidth < 768 && window.innerHeight > 760) item.style.height = `58px`;
         });
         item.classList.add('active');
-        if (window.innerWidth < 768 && window.innerHeight > 760) item.style.height = `${window.innerHeight - 242}px`;
+        if (window.innerWidth < 768 && window.innerHeight > 760) item.style.height = `${window.innerHeight - 250}px`;
       });
     });
   },[]);  
